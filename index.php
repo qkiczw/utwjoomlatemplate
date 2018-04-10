0c8b9744c0c646a3f89ce9e15c3b0479b8db9ce9
Łukasz <lukaszsztormowski@gmail.com>
2018-04-10 14:08:39 +0200

    menu test code added

--- index.php
+++ index.php
@@ -25,14 +25,31 @@
     <!--Whole site container-->
     <div class="container-fluid">
         <!--Header-->
-        <header>
-           <div class="container-fluid header-content-area">
-                <div class="row">
-                    <div class="col-sm-12 col-md-3">Logo</div>
-                    <div class="col-sm-12 col-md-9">NAzwa Szkoly</div>
+        <section id="section-header">
+            <header>
+                <div class="container-fluid header-content-area">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-3 header-logo">Logo</div>
+                            <div class="col-sm-12 col-md-9 header-school-name">NAzwa Szkoly</div>
+                        </div>
                 </div>
-           </div>
-        </header>
+                <!-- Navigation menu -->
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Features</a>
+                        <a class="nav-item nav-link" href="#">Pricing</a>
+                        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </section>
         <!-- Main site content -->
         <main>
         
