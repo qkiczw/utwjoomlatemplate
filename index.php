aadd1d92aef05e44800decafef6e3673e482836a
Łukasz <lukaszsztormowski@gmail.com>
2018-04-10 10:47:30 +0200

    change from container to div class=

--- index.php
+++ index.php
@@ -18,79 +18,95 @@
  
 <body> 
         <section id="header">
-            <container>
+            <div class="container-fluid">
                 <row>
                     <header clas="col-12">
                         <img src="temlplates/<?php echo $this->template; ?>/images/header-bg.jpg" class="img-fluid" alt="CKU header image">
                         <!-- <jdoc:include type="modules" name="header" style="xhtml" /> -->
                     </header>
                 </row>
-            </container>
+            </div>
         </section>
         <section id="nav">
-            <container>
+            <div class="container">
                 <row>
-                    <nav class="main-nav">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                    </div>
+                </div>
+                </nav>
+
+
+                    <!-- <nav class="main-nav">
                         <jdoc:include type="modules" name="nav" style="xhtml" /> 
-                    </nav>
+                    </nav> -->
                 </row>
-            </container>
+            </div>
         </section>
         <main>
         <section id="last-news">
-                <container>
+                <div class="container">
                     <row>
                         <div class="col-12 last-news">
                             <jdoc:include type="modules" name="lastnews" style="xhtml" /> 
                         </div>
                     </row>
-                </container>
+                </div>
             </section>
             <section id="main-container">
-                <container>
+                <div class="container">
                     <row>
                         <div class="col-12">
                         <jdoc:include type="component" /> 
                         </div>
                     </row>
-                </container>
+                </div>
             </section>
             <section id="aboutus">
-                <container>
+                <div class="container">
                     <row>
                         <div class="col-12 main-about-us">
                             <jdoc:include type="modules" name="mainaboutus" style="xhtml" />  
                         </div>
                     </row>
-                </container>
+                </div>
             </section>
             <section id="mainschedule">
-                <container>
+                <div class="container">
                     <row>
                         <div class="col-12 main-schedule">
                             <jdoc:include type="modules" name="mainschedule" style="xhtml"/> 
                         </div>
                     </row>
-                </container>
+                </div>
             </section>
             <section>
-                <container>
+                <div class="container">
                     <row>
                         <div class="col-12 main-contact">
                             <jdoc:include type="modules" name="maincontact" style="xhtml"/> 
                         </div>
                     </row>
-                </container>
+                </div>
             </section>
         </main>
         <section id="footer">
-                <container>
+                <div class="container">
                     <row>
                         <footer class="col-12 footer">
                             <jdoc:include type="modules" name="footer" style="xhtml"/> 
                         </footer>
                     </row>
-                </container>
+                </div>
             </section>
 
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
