5489324f0a83e15d6e33235b5a3c950e5f25cafa
Łukasz <lukaszsztormowski@gmail.com>
2018-04-10 14:16:02 +0200

    Change in code

--- index.php
+++ index.php
@@ -35,16 +35,16 @@
                 </div>
                 <!-- Navigation menu -->
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">UTW</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                        <a class="nav-item nav-link" href="#">Features</a>
-                        <a class="nav-item nav-link" href="#">Pricing</a>
-                        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-item nav-link active" href="#">Start <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">O nas</a>
+                        <a class="nav-item nav-link" href="#">Aktualnośći</a>
+                        <a class="nav-item nav-link disabled" href="#">Plan</a>
                         </div>
                     </div>
                 </nav>
