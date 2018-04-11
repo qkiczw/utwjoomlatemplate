0939636a8f6a8abada396942fede8d011285111c
Łukasz <lukaszsztormowski@gmail.com>
2018-04-11 12:32:43 +0200

    Fotter added. Styling schedule icons

--- index.php
+++ index.php
@@ -107,12 +107,16 @@
                     </div>
             </section>
         </main>
-        <!--Footer-->
-        <footer>
-            <section id="footer">
-               <center><p>Łukasz Sztormowski &copy; 2018</p></center>
+        <!-- Footer -->
+        <section id="main-footer-section">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-12 footer">
+                            <jdoc:include type="modules" name="footer" style="xhtml" /> 
+                            </div>
+                        </div>
+                    </div>
             </section>
-        </footer>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
