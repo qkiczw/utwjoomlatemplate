cae113bb2017752bc6a1ac406ebca1b269564d20
Łukasz <lukaszsztormowski@gmail.com>
2018-04-12 12:06:19 +0200

    Change sign $ to jQuery (joomla conflict)

--- index.php
+++ index.php
@@ -109,22 +109,23 @@
         </main>
         <!-- Footer -->
         <section id="main-footer-section">
-                    <footer>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-sm-12 footer">
-                                <jdoc:include type="modules" name="footer" style="xhtml" /> 
-                                </div>
-                            </div>
+            <footer>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12 footer">
+                            <jdoc:include type="modules" name="footer" style="xhtml" /> 
                         </div>
-                    </footer>
-            </section>
+                    </div>
+                </div>
+            </footer>
+        </section>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <?php
-    JHtml::script(Juri::base() . 'templates/ckutest2018/js/my-jquery.js');
+        JHtml::script(Juri::base() . 'templates/ckutest2018/js/my-jquery.js');
     ?>
 
 </body>
--- js/my-jquery.js
+++ js/my-jquery.js
@@ -1,1 +1,8 @@
-console.log('connection test')
+// You might need this, usually it's autoloaded   
+jQuery.noConflict();
+
+// Replace $ with jQuery
+jQuery(document).ready(function() {
+    let JQueryscheduleIcons = jQuery('.schedule-icons');
+   JQueryscheduleIcons.on('click', function() {console.log('test klika!!!')})
+});
