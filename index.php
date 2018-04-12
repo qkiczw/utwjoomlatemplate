7b1179ded17cec7541dabe0821dbc65612eb275f
Łukasz <lukaszsztormowski@gmail.com>
2018-04-12 11:47:10 +0200

    jquery custom empty file installed

--- index.php
+++ index.php
@@ -109,17 +109,23 @@
         </main>
         <!-- Footer -->
         <section id="main-footer-section">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-12 footer">
-                            <jdoc:include type="modules" name="footer" style="xhtml" /> 
+                    <footer>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-sm-12 footer">
+                                <jdoc:include type="modules" name="footer" style="xhtml" /> 
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </footer>
             </section>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <?php
+    JHtml::script(Juri::base() . 'templates/ckutest2018/js/my-jquery.js');
+    ?>
+
 </body>
 </html>
