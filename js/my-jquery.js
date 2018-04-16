b5ea8f7a25dcba4ada5690c372e0d2fc5eb23261
Łukasz <lukaszsztormowski@gmail.com>
2018-04-16 11:38:05 +0200

    change in code - testing scpript added

--- js/my-jquery.js
+++ js/my-jquery.js
@@ -6,3 +6,14 @@ jQuery(document).ready(function() {
     let JQueryscheduleIcons = jQuery('.schedule-icons');
    JQueryscheduleIcons.on('click', function() {console.log('test klika!!!')})
 });
+
+const jQwindow = jQuery(window);
+
+function testme() {
+    console.log(jQwindow.scrollTop())
+    if(jQwindow.scrollTop() > 500){
+        cosnole.log('tu jest 500');
+    }
+};
+
+jQwindow.on('scroll', testme);
