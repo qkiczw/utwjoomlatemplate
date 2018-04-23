17ebc88970f8779c93531b032bcdef0ce68eb9b4
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-04-23 13:22:28 +0200

    now navbar icon is showing properly

--- js/my-jquery.js
+++ js/my-jquery.js
@@ -12,12 +12,14 @@ jQuery(document).ready(function() {
         if (jQwindow.scrollTop() > 400){
             jQuery('.navbar').addClass('fixed-top');
             jQuery('.about-us-img').animate({left: '0px'}, "slow");
-            jQuery('.utw-logo-min').css('display', 'block');
-            jQuery('.navbar-brand-text').css('display', 'none');
+            // jQuery('.utw-logo-min').css('display', 'block');
+            jQuery('.utw-logo-min').fadeIn('slow');
+            jQuery('.navbar-brand-text').fadeOut('fast');
+            
         } else{
             jQuery('.navbar').removeClass('fixed-top');
-            jQuery('.utw-logo-min').css('display', 'none');
-            jQuery('.navbar-brand-text').css('display', 'block');
+            jQuery('.utw-logo-min').fadeOut();
+            jQuery('.navbar-brand-text').fadeIn();
         }
     }
     jQwindow.on('scroll', setMenuFixed);
